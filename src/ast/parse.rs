1e235fbcbb324a4774867a0d8053845f1d50f1c9
memoryleak47 <memoryleak47@protonmail.ch>
2018-03-07 19:39:33 +0100

    implemented comment-parsing

--- src/ast/parse.rs
+++ src/ast/parse.rs
@@ -1,21 +1,55 @@
 use ast::{Ast, AstNode, ConstraintDef, LetExpression};
 
-use nom::{IResult, space, alpha, alphanumeric, digit, multispace};
+use nom::{IResult, alpha, multispace};
 use std::str::from_utf8;
+
+named!(parse_one_line_comment,
+	do_parse!(
+		tag!("#") >>
+		text: take_until!("\n") >>
+		tag!("\n") >>
+		(text)
+	)
+);
+
+named!(parse_multiline_comment,
+	do_parse!(
+		tag!("#{") >>
+		text: take_until!("}#") >>
+		tag!("}#") >>
+		(text)
+	)
+);
+
+named!(ignore1<Vec<&[u8]>>,
+	many1!(alt!(
+		multispace
+		| parse_multiline_comment
+		| parse_one_line_comment
+	))
+);
+
+named!(ignore0<Vec<&[u8]>>,
+	many0!(alt!(
+		multispace
+		| parse_multiline_comment
+		| parse_one_line_comment
+	))
+);
 
 named!(parse_constraint<AstNode>,
     do_parse!(
         tag!("constraint") >>
-        multispace >>
+        ignore1 >>
         name: map_res!(
             alpha,
             from_utf8
         ) >>
-        opt!(multispace) >>
+        ignore0 >>
         tag!("{") >>
-        opt!(multispace) >>
+        ignore0 >>
         tag!("}") >>
-        opt!(multispace) >>
+        ignore0 >>
         (AstNode::ConstraintDef(ConstraintDef { name: String::from(name) }))
     )
 );
@@ -23,14 +57,14 @@ named!(parse_constraint<AstNode>,
 named!(parse_let<AstNode>,
 	do_parse!(
 		tag!("let") >>
-		multispace >>
+		ignore1 >>
 		name: map_res!(
 			alpha,
 			from_utf8
 		) >>
-		opt!(multispace) >>
+		ignore0 >>
 		tag!(";") >>
-        opt!(multispace) >>
+        ignore0 >>
 		(AstNode::LetExpression(LetExpression { name: String::from(name) }))
 	)
 );
@@ -41,6 +75,7 @@ named!(parse_ast_node<AstNode>,
 
 named!(parse_ast<Ast>,
 	do_parse!(
+		ignore0 >>
 		nodes: many0!(parse_ast_node) >>
 		(Ast { nodes })
 	)
@@ -51,7 +86,10 @@ pub fn parse(s: String) -> Result<Ast, String> {
 	println!("{:?}", parsed);
 
 	match parsed {
-		IResult::Done(_, x) => return Ok(x),
+		IResult::Done(rest, x) => {
+			if rest.is_empty() { return Ok(x); }
+			return Err(format!("Couldn't parse {:?}", from_utf8(rest)));
+		},
 		_ => panic!("TODO")
 	}
 }
