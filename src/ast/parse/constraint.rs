e20472fa6ebd6463487743c894bf78c240c74f6d
memoryleak47 <memoryleak47@protonmail.ch>
2018-03-12 11:48:28 +0100

    implemented CExpr::Or parsing

--- src/ast/parse/constraint.rs
+++ src/ast/parse/constraint.rs
@@ -26,8 +26,42 @@ named!(parse_c_expr_block<CExpr>,
 	)
 );
 
-named!(pub parse_c_expr<CExpr>, // TODO add And / Or
-	alt!(parse_c_expr_named | parse_c_expr_block)
+named!(parse_c_expr_conjunction<CExpr>,
+	do_parse!(
+		items: separated_list!(
+			do_parse!(char!('|') >> ignore0 >> (())), // TODO alt!(char!('|') | char!('&'))
+			parse_c_expr_no_conjunction
+		) >>
+        ignore0 >>
+		({
+			if items.len() <= 1 { return ::nom::IResult::Error(::nom::ErrorKind::Custom(42)); }
+
+			let mut items = items;
+
+			let first = items.remove(0);
+			items.into_iter()
+				.fold(first, |x, y| CExpr::Or(Box::new(x), Box::new(y)))
+		})
+	)
+);
+
+named!(parse_c_expr_inner<CExpr>,
+	do_parse!(
+		tag!("(") >>
+		ignore0 >>
+		cexpr: parse_c_expr >>
+		tag!(")") >>
+		ignore0 >>
+		(cexpr)
+	)
+);
+
+named!(pub parse_c_expr_no_conjunction<CExpr>,
+	alt!(parse_c_expr_inner | parse_c_expr_named | parse_c_expr_block)
+);
+
+named!(pub parse_c_expr<CExpr>,
+	alt!(parse_c_expr_conjunction | parse_c_expr_no_conjunction)
 );
 
 named!(pub parse_constraint_def<AstNode>,
