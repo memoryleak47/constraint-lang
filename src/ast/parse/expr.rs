797caab01c38a646596aaa184f390c041b785b4f
memoryleak47 <memoryleak47@protonmail.ch>
2018-03-12 14:21:38 +0100

    coded integral number parsing

--- src/ast/parse/expr.rs
+++ src/ast/parse/expr.rs
@@ -1,11 +1,44 @@
 use ast::*;
 use super::ignore::ignore0;
 
-use nom::IResult;
+use nom::{IResult, digit};
 
-pub fn parse_expr(data: &[u8]) -> IResult<&[u8], Expr> {
-	unimplemented!()
-}
+use std::str::from_utf8;
+
+named!(parse_expr_pos_int<f64>,
+	do_parse!(
+		d: digit >>
+		ignore0 >>
+		(from_utf8(d).unwrap().parse().unwrap())
+	)
+);
+
+named!(parse_expr_pos_num<f64>,
+	alt!( parse_expr_pos_int ) // TODO add floating point parser!
+);
+
+named!(parse_expr_num<Expr>,
+	do_parse!(
+		negative: opt!(char!('-')) >>
+		ignore0 >>
+		num: parse_expr_pos_num >>
+		ignore0 >>
+		({
+			if negative.is_some() {
+				Expr::Num(-num)
+			} else {
+				Expr::Num(num)
+			}
+		})
+	)
+);
+
+named!(pub parse_expr<Expr>,
+	do_parse!(
+		x: parse_expr_num >>
+		(x)
+	)
+);
 
 named!(pub parse_expr_statement<AstNode>,
 	do_parse!(
