cdf21764e28af7e8a7f9278f80ccd27c19461b35
memoryleak47 <memoryleak47@protonmail.ch>
2018-03-13 11:22:43 +0100

    fixed fun_call parsing

--- src/ast/parse/expr/fun_call.rs
+++ src/ast/parse/expr/fun_call.rs
@@ -3,23 +3,34 @@ use ast::*;
 use super::super::ignore::ignore0;
 use super::super::expr::{parse_expr_non_fun_call, parse_expr};
 
-named!(pub parse_expr_fun_call<Expr>,
+// This parses an expression, and checks whether it's a function which will be directly executed, in this case, the arguments will be parsed
+named!(pub parse_expr_opt_fun_call<Expr>,
 	do_parse!(
 		fun: parse_expr_non_fun_call >>
 		ignore0 >>
-		tag!("(") >>
-		ignore0 >>
-		args: separated_list_complete!(
-			do_parse!(tag!(",") >> ignore0 >> (())),
-			parse_expr
+		opt_args: opt!(
+			do_parse!(
+				tag!("(") >>
+				ignore0 >>
+				args: separated_list_complete!(
+					do_parse!(tag!(",") >> ignore0 >> (())),
+					parse_expr
+				) >>
+				tag!(")") >>
+				ignore0 >>
+				(args)
+			)
 		) >>
-		tag!(")") >>
-		ignore0 >>
-		(Expr::FunCall { fun: Box::new(fun), args })
+		({
+			match opt_args {
+				Some(args) => Expr::FunCall { fun: Box::new(fun), args },
+				None => fun
+			}
+		})
 	)
 );
 
 #[test]
 fn test_parse_expr_fun_call() {
-	assert!(parse_expr_fun_call("2(2,3)".as_bytes()).unwrap().0.is_empty());
+	assert!(parse_expr_opt_fun_call("2(2,3)".as_bytes()).unwrap().0.is_empty());
 }
--- src/ast/parse/expr/mod.rs
+++ src/ast/parse/expr/mod.rs
@@ -2,18 +2,15 @@ mod num;
 mod fun_call;
 
 use self::num::parse_expr_num;
-use self::fun_call::parse_expr_fun_call;
 
 use ast::*;
 use super::ignore::ignore0;
 
-named!(pub parse_expr<Expr>,
-	alt_complete!( parse_expr_num | parse_expr_fun_call )
-);
-
 named!(pub parse_expr_non_fun_call<Expr>,
 	alt_complete!( parse_expr_num )
 );
+
+pub use self::fun_call::parse_expr_opt_fun_call as parse_expr;
 
 named!(pub parse_expr_statement<AstNode>,
 	do_parse!(
